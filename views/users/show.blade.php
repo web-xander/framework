7d028c61cce0237af36b90c631c223e29def040f
Alejandro <snackbarchat@gmail.com>
2018-09-14 17:07:16 -0300

    add DatabaseDriver extends DoctrineDatabaseDriver

---
+++ views/users/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to WebXander Framework</title>
+	
+</head>
+<body>
+	<h1>Welcome to WebXander Framework</h1>
+	
+	<p>ID:{{$user->getId()}} - {{$user->firstname}}</p>
+	
+
+</body>
+</html>
